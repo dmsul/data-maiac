a2981c9595415273ef35f48c606635f42f69b3da
Daniel Sullivan <sullydm@gmail.com>
2019-08-05 20:56:53 -0400

    Flatten AOD

--- data_maiac/clean/download.py
+++ data_maiac/clean/download.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from typing import List, Optional
+
 from bs4 import BeautifulSoup
 import requests
 
--- data_maiac/clean/raw.py
+++ data_maiac/clean/raw.py
@@ -1,8 +1,31 @@
-from clean.download import hdf_local_filepath
+from glob import glob
+
+import numpy as np
 from pyhdf.SD import SD, SDC
 
+from util.env import src_path
+from clean.download import hdf_local_filepath
 
-def load_hdf(date, filename):
+
+def aod47_flat(hdf: SD) -> np.ndarray:
+    # See User Guide page 12
+    # https://lpdaac.usgs.gov/documents/110/MCD19_User_Guide_V6.pdf
+    aod_dataset = 'Optical_Depth_047'
+    fill_value = -28672
+    scale_value = 0.001
+
+    aod_table = hdf.select(aod_dataset)
+    aod = aod_table[:].astype(np.float32)
+
+    aod[aod == fill_value] = np.nan
+    aod = np.nanmean(aod, axis=0)
+
+    aod *= scale_value
+
+    return aod
+
+
+def load_hdf(date: str, filename: str) -> SD:
     hdf_path = hdf_local_filepath(date, filename)
     hdf = SD(hdf_path, SDC.READ)
     return hdf
@@ -10,6 +33,8 @@ def load_hdf(date, filename):
 
 if __name__ == '__main__':
     date = '2015.01.01'
-    filename = 'MCD19A2.A2015001.h08v05.006.2018101212324.hdf'
+    filelist = glob(src_path(date, '*.hdf'))
+    filename = filelist[0]
     hdf = load_hdf(date, filename)
-    print(hdf)
+
+    aod = aod47_flat(hdf)
