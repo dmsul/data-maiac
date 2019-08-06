df783dbd4740d2772032c61a60f2f7dd22e0eb96
Daniel Sullivan <sullydm@gmail.com>
2019-08-05 22:51:18 -0400

    Finish 'to df' stuff

--- data_maiac/clean/raw.py
+++ data_maiac/clean/raw.py
@@ -2,10 +2,45 @@ from glob import glob
 from typing import Tuple
 
 import numpy as np
+import pandas as pd
 from pyhdf.SD import SD, SDC
 
-from util.env import src_path
+from util.gis import sinu
 from clean.download import hdf_local_filepath
+
+SCALE_VALUE_47 = 0.001
+
+
+def aod47_day_df(date: str) -> pd.DataFrame:
+    filelist = glob(hdf_local_filepath(date, '*.hdf'))
+    dfs = [aod47_day_grid_df(f) for f in filelist]
+    df = pd.concat(dfs, ignore_index=True)
+
+    df['aod'] /= SCALE_VALUE_47
+    df['aod'] = df['aod'].astype(np.int16)
+
+    df['x'], df['y'] = sinu(df['x'].values, df['y'].values, inverse=True)
+    for col in ('x', 'y'):
+        df[col] = df[col].astype(np.float32)
+
+    return df
+
+
+def aod47_day_grid_df(filepath: str) -> pd.DataFrame:
+    hdf = load_hdf(filepath)
+
+    aod = aod47_flat(hdf)
+    ul, lr = get_corners(hdf)
+    x, y = get_xy(aod, ul, lr)
+
+    df = aod47_combine_todf(aod, x, y)
+
+    return df
+
+
+def load_hdf(filepath: str) -> SD:
+    hdf = SD(filepath, SDC.READ)
+    return hdf
 
 
 def aod47_flat(hdf: SD) -> np.ndarray:
@@ -13,7 +48,7 @@ def aod47_flat(hdf: SD) -> np.ndarray:
     # https://lpdaac.usgs.gov/documents/110/MCD19_User_Guide_V6.pdf
     aod_dataset = 'Optical_Depth_047'
     fill_value = -28672
-    scale_value = 0.001
+    SCALE_VALUE_47 = 0.001
 
     aod_table = hdf.select(aod_dataset)
     aod = aod_table[:].astype(np.float32)
@@ -21,7 +56,7 @@ def aod47_flat(hdf: SD) -> np.ndarray:
     aod[aod == fill_value] = np.nan
     aod = np.nanmean(aod, axis=0)
 
-    aod *= scale_value
+    aod *= SCALE_VALUE_47
 
     return aod
 
@@ -44,17 +79,40 @@ def get_corners(hdf: SD) -> Tuple[Tuple[float, float], Tuple[float, float]]:
     return UpperLeftPointMtrs, LowerRightMtrs
 
 
-def load_hdf(date: str, filename: str) -> SD:
-    hdf_path = hdf_local_filepath(date, filename)
-    hdf = SD(hdf_path, SDC.READ)
-    return hdf
+def get_xy(aod: np.ndarray,
+           ul: Tuple[float, float],
+           lr: Tuple[float, float]) -> Tuple[np.ndarray, np.ndarray]:
+    N = aod.shape[0]    # Should be 1200
+    diff = abs(ul[0] - lr[0]) / N
+    diff_half = diff / 2
+
+    xs = [ul[0] + diff_half + (diff * i) for i in range(N)]
+    ys = [ul[1] - diff_half - (diff * j) for j in range(N)]
+
+    master_x, master_y = np.meshgrid(xs, ys)
+
+    return master_x, master_y
 
 
-if __name__ == '__main__':
-    date = '2015.01.01'
-    filelist = glob(src_path(date, '*.hdf'))
-    filename = filelist[0]
-    hdf = load_hdf(date, filename)
+def aod47_combine_todf(aod: np.ndarray,
+                       x: np.ndarray,
+                       y: np.ndarray) -> pd.DataFrame:
+    aod_nonmiss = ~np.isnan(aod)
+    aod2 = aod[aod_nonmiss].flatten()
+    x2 = x[aod_nonmiss].flatten()
+    y2 = y[aod_nonmiss].flatten()
 
-    aod = aod47_flat(hdf)
-    ul, lr = get_corners(hdf)
+    df = pd.DataFrame(
+        np.hstack(
+            (aod2.reshape(-1, 1),
+             x2.reshape(-1, 1),
+             y2.reshape(-1, 1)
+             )
+        ), columns=['aod', 'x', 'y']
+    )
+
+    return df
+
+
+if __name__ == "__main__":
+    df = aod47_day_df('2015.01.01')
