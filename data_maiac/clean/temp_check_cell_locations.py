a27a0e457603eeedefe063ac43527b12ec256484
meashe <ashenfarb@rff.org>
2019-08-07 09:24:50 -0400

    check all downloaded files instead of hard coded files

--- data_maiac/clean/temp_check_cell_locations.py
+++ data_maiac/clean/temp_check_cell_locations.py
@@ -1,5 +1,6 @@
-from glob import glob
+import os
 import sys
+from glob import glob
 
 from pyhdf.SD import SD, SDC
 
@@ -13,20 +14,22 @@ def read_hdf(hdf_path):
 
 
 if __name__ == "__main__":
-    day1 = glob(src_path('2000.06.05', '*.hdf'))
-    day2 = glob(src_path('2015.04.11', '*.hdf'))
+    days = [dns for rt, dns, fns in os.walk(src_path())][0]
 
-    for i in range(len(day1)):
-        ul1, lr1 = get_corners(read_hdf(day1[i]))
-        ul2, lr2 = get_corners(read_hdf(day2[i]))
+    for i in range(len(days) - 1):
+        day1 = glob(src_path(days[i], '*.hdf'))
+        day2 = glob(src_path(days[i + 1], '*.hdf'))
+        for j in range(len(day1)):
+            ul1, lr1 = get_corners(read_hdf(day1[j]))
+            ul2, lr2 = get_corners(read_hdf(day2[j]))
 
-        try:
-            assert (ul1 == ul2)
-        except:
-            print(f'test {i} failed because {ul1} =/= {ul2}')
-            sys.exit()
-        try:
-            assert (lr1 == lr2)
-        except:
-            print(f'test {i} failed because {lr1} =/= {lr2}')
-            sys.exit()
+            try:
+                assert (ul1 == ul2)
+            except:
+                print(f'test {i} failed because {ul1} =/= {ul2}')
+                sys.exit()
+            try:
+                assert (lr1 == lr2)
+            except:
+                print(f'test {i} failed because {lr1} =/= {lr2}')
+                sys.exit()
